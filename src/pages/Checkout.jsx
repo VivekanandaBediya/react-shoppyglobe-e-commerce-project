2aad75f5ed23cdbf3261cfe32758960b3359bc4a
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2026-03-09 17:36:40 +0530

    Checkout page is implemented

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,24 @@
-function Checkout(){
-    return(
-        <>
-            <h1>This page is under construction</h1>
-        </>
-    )
+import { useNavigate } from "react-router-dom";
+
+function Checkout() {
+  const navigate = useNavigate();
+
+  const placeOrder = () => {
+    alert("Order placed");
+
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h2>Checkout</h2>
+
+      <input placeholder="Name" />
+      <input placeholder="Address" />
+
+      <button onClick={placeOrder}>Place Order</button>
+    </div>
+  );
 }
 
 export default Checkout;
