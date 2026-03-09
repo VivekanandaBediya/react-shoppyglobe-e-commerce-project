92ab2355d2d120658c1601ff222e86073ee8c167
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2026-03-09 17:31:52 +0530

    Cart page is implemented

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,18 @@
-function Cart(){
-    return(
-        <>
-            <h1>This page is under construction</h1>
-        </>
-    )
+import { useSelector } from "react-redux";
+import CartItem from "../components/CartItem";
+
+function Cart() {
+  const items = useSelector((state) => state.cart.items);
+
+  return (
+    <div>
+      <h2>Your Cart</h2>
+
+      {items.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
 
 export default Cart;
