fa9b6c50a08d67fdd94e54d6fb5de7704581fe52
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2026-03-09 17:34:23 +0530

    CartItem page implemented

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,18 @@
-function CartItem(){
-    return(
-        <>
-            <h1>This page is under construction</h1>
-        </>
-    )
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../redux/cartSlice";
+
+function CartItem({ item }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="cart-item">
+      <h3>{item.title}</h3>
+
+      <p>Qty : {item.quantity}</p>
+
+      <button onClick={() => dispatch(removeFromCart(item.id))}>Remove</button>
+    </div>
+  );
 }
 
 export default CartItem;
