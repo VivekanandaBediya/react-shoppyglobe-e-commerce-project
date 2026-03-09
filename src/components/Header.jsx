a7994be18ee9e49f9c6acb3a69d9520c9c3a0f1e
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2026-03-09 17:25:52 +0530

    Header component implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-function Header(){
-    return(
-        <>
-            <h1>This page is under construction</h1>
-        </>
-    )
+import { Link } from "react-router-dom";
+import "../App.css"
+
+function Header() {
+  return (
+    <header className="header">
+      <h2>ShoppyGlobe</h2>
+
+      <nav>
+        <Link to="/" className="nav-list">Home</Link>
+        <Link to="/cart" className="nav-list">Cart</Link>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,10 @@
-function NotFound(){
-    return(
-        <>
-            <h1>This page is under construction</h1>
-        </>
-    )
+function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
 }
 
 export default NotFound;
