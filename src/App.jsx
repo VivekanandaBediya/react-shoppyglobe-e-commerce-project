679e6b0e7a5890255d1b282ac7ed7c8eb26abcef
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2026-03-10 13:54:43 +0530

    App page update

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,13 @@ import Header from "./components/Header";
 import { Suspense } from "react";
 
 function App(){
-
- return(
-
+  return(
   <div>
-
    <Header/>
-
    <Suspense fallback={<h2>Loading...</h2>}>
-
      <Outlet/>
-
    </Suspense>
-
   </div>
-
  )
 
 }
