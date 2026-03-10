61296771a4027efda898bd24a36704f59e912990
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2026-03-10 13:42:36 +0530

    CartItem component updated

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,40 +1,23 @@
 import { useDispatch } from "react-redux";
 
-import {
- removeFromCart,
- increaseQty,
- decreaseQty
-} from "../redux/cartSlice";
+import {removeFromCart,increaseQty,decreaseQty} from "../redux/cartSlice";
 
 function CartItem({item}){
 
  const dispatch = useDispatch();
 
  return(
-
   <div>
 
    <h3>{item.title}</h3>
 
    <p>Quantity: {item.quantity}</p>
 
-   <button
-    onClick={()=>dispatch(increaseQty(item.id))}
-   >
-    +
-   </button>
+   <button onClick={()=>dispatch(increaseQty(item.id))}>+</button>
 
-   <button
-    onClick={()=>dispatch(decreaseQty(item.id))}
-   >
-    -
-   </button>
+   <button onClick={()=>dispatch(decreaseQty(item.id))}>-</button>
 
-   <button
-    onClick={()=>dispatch(removeFromCart(item.id))}
-   >
-    Remove
-   </button>
+   <button onClick={()=>dispatch(removeFromCart(item.id))}>Remove</button>
 
   </div>
 
